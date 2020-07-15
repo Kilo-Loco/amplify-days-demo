053e068e9de6fa95a330fd4587da5f101f6f0c4e
Kilo Loco <kiloloco@me.com>
2020-07-14 22:31:16 -0700

    Initial Commit

---
+++ AmplifyDaysPhoto/AmplifyDaysPhotoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AmplifyDaysPhotoApp.swift
+//  AmplifyDaysPhoto
+//
+//  Created by Kyle Lee on 7/14/20.
+//
+
+import SwiftUI
+
+@main
+struct AmplifyDaysPhotoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
